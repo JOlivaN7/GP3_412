12be6264859690392bd34d0ec3cc76bff2e9966d
JOlivaN7 <joliva@tamu.edu>
2020-03-02 02:56:56 -0600

    output correct data to text file
    main.py added ability to output web data to AWSoutput.txt file correctly

--- Main.py
+++ Main.py
@@ -1,15 +1,24 @@
 ### Import data and read,...
 
 
-import urllib.request
+from urllib.request import urlopen
 
 
 ### Pull data from AWS database
-def main():
-  webUrl = urllib.request.urlopen("https://s3.amazonaws.com/tcmg476/http_access_log")
-  print("result: " + str(webUrl.getcode()))
-  data = webUrl.read()
-  print(data)
 
-if __name__ == "__main__":
-  main()
+AWSurl = "https://s3.amazonaws.com/tcmg476/http_access_log"
+data1 = urlopen(AWSurl)
+strData = data1.read()
+s1 = str(strData, encoding = 'utf-8')
+OutP = open("AWSoutput.txt", "w")
+
+#for line in s1:
+ #   fields = line.split("local")
+  #  OutP.write(type)
+
+#m = s1.split("local", 2)
+OutP.write(s1)
+
+
+OutP.close()
+
